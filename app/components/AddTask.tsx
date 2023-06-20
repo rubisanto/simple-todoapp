7251530d653315a76540e6df5a0b8e4c0bfb7cf0
rubisanto <rubisanto06@gmail.com>
2023-06-20 17:11:39 +0200

    init + bouton basic

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,13 @@
+import { AiOutlinePlus } from "react-icons/ai";
+
+export const AddTask = () => {
+  return (
+    <div>
+      <button className="btn btn-primary w-full">
+        Add new task <AiOutlinePlus className="ml-2" size={24} />
+      </button>
+    </div>
+  );
+};
+
+export default AddTask;
