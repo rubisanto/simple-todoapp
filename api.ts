ea2ab06e9517376b961ca5f33b94956125589d48
rubisanto <rubisanto06@gmail.com>
2023-06-26 10:37:47 +0200

    addTodoAPI + uuid

--- api.ts
+++ api.ts
@@ -5,7 +5,19 @@ const baseUrl = 'http://localhost:3001'
 // the promise is resolved with the response of the request
 // the type is inferred from the return type of the function
 export const getAllTodos = async (): Promise<ITask[]> => {
-    const res = await fetch(`${baseUrl}/tasks`)
+    const res = await fetch(`${baseUrl}/tasks`, {cache: 'no-store'})
     const todos = await res.json()
     return todos
+}
+
+export const addTodo = async (todo: ITask): Promise<ITask> => {
+    const res = await fetch(`${baseUrl}/tasks`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    const newTodo = await res.json()
+    return newTodo
 }
--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -3,15 +3,25 @@
 import { AiOutlinePlus } from "react-icons/ai";
 import Modal from "./Modal";
 import { FormEventHandler, useState } from "react";
+import { addTodo } from "@/api";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 
 export const AddTask = () => {
+  const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [newTaskValue, setNewTaskValue] = useState<string>(""); // [1]
-  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = (
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (
     e: React.FormEvent<HTMLFormElement>
   ) => {
     e.preventDefault();
+    await addTodo({
+      id: uuidv4(),
+      text: newTaskValue,
+    });
     setNewTaskValue("");
+    setModalOpen(false);
+    router.refresh();
   };
   return (
     <div>
