075a60e5692fbd612f8b2f42f7639a3536052674
rubisanto <rubisanto06@gmail.com>
2023-06-26 17:17:06 +0200

    deleteTask : done

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -5,7 +5,7 @@ import { FormEventHandler, useState } from "react";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import Modal from "./Modal";
 import { useRouter } from "next/navigation";
-import { editTodo } from "@/api";
+import { deleteTodo, editTodo } from "@/api";
 
 interface TaskProps {
   task: ITask;
@@ -25,8 +25,13 @@ const Task: React.FC<TaskProps> = ({ task }) => {
       id: task.id,
       text: taskToEdit,
     });
-    setTaskToEdit("");
     setOpenModalEdit(false);
+    router.refresh();
+  };
+
+  const handleDeleteTask = async (id: string) => {
+    await deleteTodo(id);
+    setOpenModalDeleted(false);
     router.refresh();
   };
 
@@ -67,7 +72,33 @@ const Task: React.FC<TaskProps> = ({ task }) => {
             </button>
           </form>
         </Modal>
-        <FiTrash2 cursor="pointer" className="text-red-500" size={25} />
+        <FiTrash2
+          onClick={() => setOpenModalDeleted(true)}
+          cursor="pointer"
+          className="text-red-500"
+          size={25}
+        />
+        <Modal modalOpen={openModalDeleted} setModalOpen={setOpenModalDeleted}>
+          <form method="dialog" className="modal-box">
+            <h3 className="text-lg">
+              Are you sure you want to delete this task ?
+            </h3>
+            <button
+              onClick={() => setOpenModalDeleted(false)}
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            >
+              ✕
+            </button>
+            <div className="modal-action">
+              <button
+                onClick={() => handleDeleteTask(task.id)}
+                className="btn btn-primary"
+              >
+                Yes
+              </button>
+            </div>
+          </form>
+        </Modal>
       </td>
     </tr>
   );
